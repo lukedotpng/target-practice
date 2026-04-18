2385b63ee129fd8366d6574b5cae6f3d89a2fb02
luke <hey@luke.town>
2026-04-18 15:09:31 -0700

    Changed base colors, font sizes and other styles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,7 +8,7 @@
 	<link rel="icon" href={asset('/images/targetpractice_logo_100x100.png')} />
 </svelte:head>
 
-<header class="header">
+<header>
 	<div class="header-contents">
 		<h1>
 			<a href={resolve('/')}>
@@ -24,7 +24,7 @@
 {@render children()}
 
 <style>
-	.header {
+	header {
 		display: flex;
 		justify-content: center;
 		padding-left: 0.5rem;
@@ -51,12 +51,10 @@
 			display: flex;
 			align-items: center;
 			height: 100%;
-			color: inherit;
 			text-decoration: none;
 
 			& > span {
-				height: fit-content;
-				margin-top: 0.8rem;
+				margin-top: 0.5rem;
 				@media (width <= 600px) {
 					display: none;
 				}
@@ -80,7 +78,6 @@
 			font-family: 'DM Sans', sans-serif;
 			font-size: var(--step-1);
 			font-weight: bold;
-			color: var(--text-white);
 
 			text-decoration: none;
 			&:hover {
@@ -90,59 +87,11 @@
 	}
 
 	:global {
-		:root {
-			color-scheme: light dark;
-			--secondary-base-color: oklch(0.78 0.12 249.59);
-			--success-base-color: oklch(75.603% 0.18029 155.021);
-			--error-base-color: oklch(0.58 0.2 25.75);
-			--neutral-color-1: oklch(0.23 0.003 240);
-			--neutral-color-2: oklch(0.3 0.004 240);
-			--neutral-color-3: oklch(0.4 0.005 240);
-			--neutral-color-4: oklch(0.54 0.006 240);
-			--neutral-color-5: oklch(0.75 0.005 240);
-			--neutral-color-6: oklch(0.85 0.004 240);
-			--neutral-color-7: oklch(0.9 0.003 240);
-			--neutral-color-8: oklch(0.94 0.002 240);
-			--neutral-color-9: oklch(0.98 0.001 240);
-
-			--primary-color: oklch(0.6391 0.1623 22.82);
-
-			--secondary-color: light-dark(
-				var(--secondary-base-color),
-				oklch(from var(--secondary-base-color) calc(l - 0.15) c h)
-			);
-			--success-color: light-dark(
-				var(--success-base-color),
-				oklch(from var(--success-base-color) calc(l - 0.04) c h)
-			);
-			--error-color: light-dark(
-				var(--error-base-color),
-				oklch(from var(--error-base-color) calc(l + 0.08) c h)
-			);
-
-			--text-light: var(--neutral-color-9);
-			--text-dark: var(--neutral-color-1);
-			--text-color: light-dark(var(--text-dark), var(--text-light));
-
-			--base-color-1: light-dark(var(--neutral-color-8), var(--neutral-color-1));
-			--base-color-2: light-dark(var(--neutral-color-7), var(--neutral-color-2));
-			--base-color-3: light-dark(var(--neutral-color-6), var(--neutral-color-3));
-
-			/* @link https://utopia.fyi/type/calculator?c=320,16,1.2,1280,20,1.333,5,2,&s=0.75|0.5|0.25,1.5|2|3|4|6,s-l&g=s,l,xl,12 */
-			--step--2: clamp(0.6944rem, 0.6914rem + 0.0151vw, 0.7035rem);
-			--step--1: clamp(0.8333rem, 0.7985rem + 0.174vw, 0.9377rem);
-			--step-0: clamp(1rem, 0.9167rem + 0.4167vw, 1.25rem);
-			--step-1: clamp(1.2rem, 1.0446rem + 0.7771vw, 1.6663rem);
-			--step-2: clamp(1.44rem, 1.1796rem + 1.3019vw, 2.2211rem);
-			--step-3: clamp(1.728rem, 1.3171rem + 2.0546vw, 2.9607rem);
-			--step-4: clamp(2.0736rem, 1.4492rem + 3.1218vw, 3.9467rem);
-			--step-5: clamp(2.4883rem, 1.5641rem + 4.621vw, 5.2609rem);
-		}
-
 		* {
 			margin: 0;
 			padding: 0;
 			font: inherit;
+			color: inherit;
 		}
 
 		*,
@@ -151,8 +100,87 @@
 			box-sizing: border-box;
 		}
 
+		html {
+			height: 100%;
+		}
+
 		body {
-			background-color: var(--base-color-1);
+			display: grid;
+			grid-template-rows: auto 1fr auto;
+			min-height: 100%;
+			background-color: var(--base-color-3);
+			color: var(--text-color);
+		}
+
+		input {
+			padding-inline: 0.15rem;
+			background-color: var(--base-color-5);
+			border-color: var(--base-color-8);
+			border-style: solid;
+			border-radius: 5px;
+			outline: none;
+
+			&:focus {
+				border-color: var(--base-color-12);
+			}
+			&:hover:not(:disabled):not(:focus) {
+				border-color: var(--base-color-10);
+			}
+			&::placeholder {
+				font-style: italic;
+			}
+		}
+
+		select {
+			background-color: var(--base-color-5);
+			border-width: 2px;
+			border-color: var(--base-color-8);
+			border-style: solid;
+			border-radius: 5px;
+			&:open {
+				border-radius: 5px 5px 0 0;
+			}
+			transition: border-radius 0.1s;
+
+			&:focus {
+				border-color: var(--base-color-12);
+			}
+			&:hover:not(:disabled):not(:focus) {
+				border-color: var(--base-color-10);
+			}
+		}
+
+		:root {
+			color-scheme: light dark;
+			--primary-color: oklch(0.6391 0.1623 22.82);
+			--secondary-color: oklch(0.7 0.12 249.59);
+			--success-color: oklch(0.7 0.18029 155.021);
+			--error-color: oklch(0.55 0.21 30);
+
+			--text-color: white;
+
+			--base-color-1: oklch(0.162 0 0);
+			--base-color-2: oklch(0.195 0 0);
+			--base-color-3: oklch(0.254 0 0);
+			--base-color-4: oklch(0.302 0 0);
+			--base-color-5: oklch(0.348 0 0);
+			--base-color-6: oklch(0.396 0 0);
+			--base-color-7: oklch(0.459 0 0);
+			--base-color-8: oklch(0.549 0 0);
+			--base-color-9: oklch(0.649 0 0);
+			--base-color-10: oklch(0.72 0 0);
+			--base-color-11: oklch(0.863 0 0);
+			--base-color-12: oklch(0.933 0 0);
+
+			/* @link https://utopia.fyi/type/calculator?c=375,16,1.2,1280,18,1.2,5,2,&s=0.75|0.5|0.25,1.5|2|3|4|6,s-l&g=s,l,xl,12 */
+			--step--2: clamp(0.6944rem, 0.6585rem + 0.1535vw, 0.7813rem);
+			--step--1: clamp(0.8333rem, 0.7902rem + 0.1842vw, 0.9375rem);
+			--step-0: clamp(1rem, 0.9482rem + 0.221vw, 1.125rem);
+			--step-1: clamp(1.2rem, 1.1378rem + 0.2652vw, 1.35rem);
+			--step-2: clamp(1.44rem, 1.3654rem + 0.3182vw, 1.62rem);
+			--step-3: clamp(1.728rem, 1.6385rem + 0.3819vw, 1.944rem);
+			--step-4: clamp(2.0736rem, 1.9662rem + 0.4583vw, 2.3328rem);
+			--step-5: clamp(2.4883rem, 2.3594rem + 0.5499vw, 2.7994rem);
 		}
 	}
 
