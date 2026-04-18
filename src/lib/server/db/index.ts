e612b3676e6e24b26e8424bd79632df5d21a9155
luke <hey@luke.town>
2026-04-18 15:04:58 -0700

    Updated drizzle to 1.0

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -7,4 +7,4 @@ if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
 const client = mysql.createPool(env.DATABASE_URL);
 
-export const db = drizzle(client, { schema, mode: 'default' });
+export const db = drizzle({ client, schema, relations: schema.relations, mode: 'default' });
